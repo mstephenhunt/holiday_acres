564f256e93fc7623775521e71ca7605389d9fb87
Matt Hunt <mstephenhunt@gmail.com>
2021-09-28 16:37:56 -0400

    installed apps

--- api/api/settings.py
+++ api/api/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "holiday_acres_api.apps.HolidayAcresApiConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
