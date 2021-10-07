cb3a6918a782a313ce810584f8f28c321a37af7c
Cliff Hunt <honduq@gmail.com>
2021-10-06 21:23:43 -0400

    condensed migrations?

--- api/holiday_acres_api/models.py
+++ api/holiday_acres_api/models.py
@@ -5,6 +5,7 @@ class User(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
 
 
 class Paddock(models.Model):
