7b3e860a2dbc5f9850725364f64ba48a0e2e79ef
Matt Hunt <mstephenhunt@gmail.com>
2021-10-10 07:41:48 -0400

    basic user test

---
+++ api/holiday_acres_api/tests/user_unit_test.py
@@ -0,0 +1,22 @@
+import pytest
+
+from holiday_acres_api.models import User
+
+"""
+Can create and fetch basic user in DB
+"""
+
+
+@pytest.mark.django_db
+def test_user_create():
+    user = User(name="Testy McTesterson")
+    user.save()
+
+    # Should only be one user in DB
+    assert User.objects.count() == 1
+
+    # Get that user -- also there's probably a better way to do this get()
+    db_user = User.objects.all()[:1].get()
+
+    # Check name
+    assert db_user.name == "Testy McTesterson"
