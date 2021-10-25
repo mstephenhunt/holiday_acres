26c2b905eb9c295a85320e3511b71b54522ddb20
Cliff Hunt <honduq@gmail.com>
2021-10-24 20:09:09 -0400

    added horse_relationships_unit_test, removed relationship test from horse_unit_test

--- api/holiday_acres_api/tests/horse_relationships_unit_test.py
+++ api/holiday_acres_api/tests/horse_relationships_unit_test.py
@@ -11,7 +11,13 @@ Can create and fetch basic horse in DB
 
 
 @pytest.mark.django_db
-def test_horse_create():
+def test_horse_relationships_create():
+    # create instance for paddock relationship
+    test_paddock = Paddock(paddock_name="test_paddock", paddock_tier=9)
+    test_paddock.save()
+    # create instance for user relationship
+    test_user = User(first_name="Testy", last_name="McTesterson", email="user@haec.com")
+    test_user.save()
     # create horse instance for testing
     horse = Horse(
         name="Firebrand",
@@ -20,19 +26,27 @@ def test_horse_create():
         feed="Lots of food",
         health="Good",
         misc_notes="Important things you should know about horses",
+        paddock=test_paddock,
+        user=test_user,
     )
     horse.save()
 
-    # Should only be one horse in DB
+    # Should only be one user in DB
     assert Horse.objects.count() == 1
 
-    # Get that horse -- also there's probably a better way to do this get()
+    # Get that user -- also there's probably a better way to do this get()
     db_horse = Horse.objects.all()[:1].get()
 
-    # Check name
+    # Do we need to assert these things if this is just a relationship test,
+    # or should "Check values" be removed?
+    # Check values
     assert db_horse.name == "Firebrand"
     assert db_horse.age == 10
     assert db_horse.tier == 0
     assert db_horse.feed == "Lots of food"
     assert db_horse.health == "Good"
     assert db_horse.misc_notes == "Important things you should know about horses"
+
+    # Check relationships
+    assert db_horse.paddock == test_paddock
+    assert db_horse.user == test_user
