51ebfa2bb3e84168cc274be36876233c3a651c29
Matt Hunt <mstephenhunt@gmail.com>
2021-11-11 09:31:21 -0500

    implement fetcher

---
+++ ui/fetcher.ts
@@ -0,0 +1,15 @@
+function responseTimeout(milliseconds: number) {
+  return new Promise<Response>((resolve) => {
+    setTimeout(() => {
+      resolve(new Response(null, { status: 408, statusText: 'UI fetch timeout' }));
+    }, milliseconds);
+  });
+}
+
+export const fetcher = async (path: string): Promise<Response> => {
+  const timeout = 5000;
+  const url = `http://localhost:8000${path}`;
+
+  // Promise.race used for either timeout or request resolution
+  return Promise.race([fetch(url.toString(), {}), responseTimeout(timeout)]);
+}
--- ui/pages/health.tsx
+++ ui/pages/health.tsx
@@ -1,4 +1,12 @@
+import useSWR from 'swr';
+import { fetcher } from '../fetcher';
+
+/**
+ * Server-side rendering for a basic health check.
+ */ 
 export default function health() {
+  const { data, error } = useSWR('/api/user', fetcher)
+
   return (
     <div>
       <p>Dummy health page</p>
