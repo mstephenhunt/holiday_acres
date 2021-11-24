ca1f7ddfefa5f2f7b2c3ab827b1159cf5150f537
Matt Hunt <mstephenhunt@gmail.com>
2021-11-23 20:15:29 -0500

    as is

--- ui/components/Header.tsx
+++ ui/components/Header.tsx
@@ -1,5 +1,20 @@
-export default function Header() {
+import styled from 'styled-components'
+
+// const Button = styled.button``
+
+// const Header = styled.header`
+//   background-color: #cc1003;
+// `;
+
+export default function HeaderComponent() {
   return (
-    <div>Dummy header</div>
+    // <Header>SomeContext</Header>
+    <header style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
+      <nav>
+        <img alt='logo'></img>
+        <a href="/">Home</a>
+        <a href="/api/health">Health</a>
+      </nav>
+    </header>
   );
 }
