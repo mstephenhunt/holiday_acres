b9cb5dd56850d0b0597e25c77a2abe85346625cc
Cliff Hunt <honduq@gmail.com>
2021-11-25 06:45:42 -0500

    test_duplicate_user, added pytest.mark to views_unit_test.py

--- api/holiday_acres_api/tests/views_unit_test.py
+++ api/holiday_acres_api/tests/views_unit_test.py
@@ -10,6 +10,7 @@ request = factory.post('/a/test/path/', data, content_type='application/json')
 """
 
 
+@pytest.mark.django_db(transaction=True)
 def test_create_user_view():
     request = RequestFactory().post("/users/register")
     response = register_account_request(request)
@@ -18,6 +19,7 @@ def test_create_user_view():
     assert response.reason_phrase == "Missing required fields to register user"
 
 
+@pytest.mark.django_db(transaction=True)
 def test_password_mismatch():
     data = {
         "username": "test_username",
@@ -33,3 +35,35 @@ def test_password_mismatch():
     response = register_account_request(request)
     assert response.status_code == 500
     assert response.reason_phrase == "Password and password confirmation don't match"
+
+
+@pytest.mark.django_db(transaction=True)
+def test_duplicate_user():
+    # register example user
+    data = {
+        "username": "test_username",
+        "password": "12345",
+        "password_confirmation": "12345",
+        "email": "anemailaddress",
+        "first_name": "Jon",
+        "last_name": "hunt",
+    }
+    request = RequestFactory().post(
+        "/users/register", data, content_type="application/json"
+    )
+    response = register_account_request(request)
+    # register same duplicate user
+    data = {
+        "username": "test_username",
+        "password": "12345",
+        "password_confirmation": "12345",
+        "email": "anemailaddress",
+        "first_name": "Jon",
+        "last_name": "hunt",
+    }
+    request = RequestFactory().post(
+        "/users/register", data, content_type="application/json"
+    )
+    response = register_account_request(request)
+    assert response.status_code == 500
+    assert response.reason_phrase == "Username already exists"
