7516b375baa0d4eeec80562d010f7ab78cabc460
Cliff Hunt <honduq@gmail.com>
2021-11-24 19:31:13 -0500

    added test_password mismatch test

--- api/holiday_acres_api/tests/views_unit_test.py
+++ api/holiday_acres_api/tests/views_unit_test.py
@@ -3,6 +3,12 @@ import pytest
 from django.test import RequestFactory
 from holiday_acres_api.views import register_account_request
 
+"""
+factory = RequestFactory()
+data = {'message':'A test message'}
+request = factory.post('/a/test/path/', data, content_type='application/json')
+"""
+
 
 def test_create_user_view():
     request = RequestFactory().post("/users/register")
@@ -10,3 +16,20 @@ def test_create_user_view():
 
     assert response.status_code == 500
     assert response.reason_phrase == "Missing required fields to register user"
+
+
+def test_password_mismatch():
+    data = {
+        "username": "test_username",
+        "password": "12345",
+        "password_confirmation": "67890",
+        "email": "anemailaddress",
+        "first_name": "Jon",
+        "last_name": "hunt",
+    }
+    request = RequestFactory().post(
+        "/users/register", data, content_type="application/json"
+    )
+    response = register_account_request(request)
+    assert response.status_code == 500
+    assert response.reason_phrase == "Password and password confirmation don't match"
