00562606eb7d08ec8a4551ebdae97d5dd3a51dcb
Matt Hunt <mstephenhunt@gmail.com>
2021-12-07 09:38:51 -0500

    removed unused components

--- ui/pages/index.tsx
+++ ui/pages/index.tsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import Album from "../src/Album";
 import Container from "@mui/material/Container";
 import ButtonAppBar from "../src/ButtonAppBar";
 import BarnSection from "../src/BarnSection";
