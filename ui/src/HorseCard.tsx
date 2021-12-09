9f26537dc768c502986046037d2e65f5f2f3a528
Matt Hunt <mstephenhunt@gmail.com>
2021-12-09 12:58:49 -0500

    as is

--- ui/src/HorseCard.tsx
+++ ui/src/HorseCard.tsx
@@ -13,34 +13,38 @@ type HorseCardProps = {
   name: string;
   stall: string;
   feed: Feed[];
+  specialInstuctions?: string;
 }
 
 export default function HorseCard(props: HorseCardProps) {
   return (
     <Grid item xs={12}>
       <Card sx={{ display: 'flex', padding: 1, boxShadow: 'rgba(0, 0, 0, 0.24) 0px 3px 8px' }}>
-        <Grid container xs={12}>
-          <Grid item xs={12}>
-            <Grid container xs={12} direction="row">
-              <Grid item xs={3}>
-                {/*Horse Picture*/}
-                <CardMedia
-                  component="img"
-                  sx={{ width: 80, height: 80, display: { xs: 'block' } }}
-                  image='https://source.unsplash.com/random'
-                  alt={props.name}
-                />
-              </Grid>
-              <Grid item xs={9}>
-                <CardContent>
-                   <HorseInfoHeading
-                     name={props.name}
-                     stall={props.stall}
-                   />
-                </CardContent>
-              </Grid>
-            </Grid>
+
+
+
+        <Grid container direction="row">
+          <Grid item xs={3}>
+            {/*Horse Picture*/}
+            <CardMedia
+              component="img"
+              sx={{ width: 80, height: 80, display: { xs: 'block' } }}
+              image='https://source.unsplash.com/random'
+              alt={props.name}
+            />
           </Grid>
+          <Grid item xs={9}>
+            <CardContent>
+               <HorseInfoHeading
+                 name={props.name}
+                 stall={props.stall}
+               />
+            </CardContent>
+          </Grid>
+
+
+
+
           <Grid item xs={12} sx={{ display: 'flex', justifyContent: 'center' }}>
             <Box sx={{ display: 'flex', width: 0.95, borderBottom: 0.8, borderColor: '#C4C4C4', marginTop: 0.5, marginBottom: 0.5 }} />
           </Grid>
@@ -53,14 +57,16 @@ export default function HorseCard(props: HorseCardProps) {
               />
             ))}
           </Grid>
-          <Grid item xs={12} sx={{ display: 'flex', justifyContent: 'center' }}>
-            <Box sx={{ display: 'flex', width: 0.95, borderBottom: 0.8, borderColor: '#C4C4C4', marginTop: 0.5, marginBottom: 0.5 }} />
-          </Grid>
-          <Grid item xs={12}>
-            <HorseSpecialInstructions
-              specialInstuctions='These are instructions'
-            />
-          </Grid>
+              <Grid item xs={12} sx={{ display: 'flex', justifyContent: 'center' }}>
+                <Box sx={{ display: 'flex', width: 0.95, borderBottom: 0.8, borderColor: '#C4C4C4', marginTop: 0.5, marginBottom: 0.5 }} />
+              </Grid>
+          { props.specialInstuctions &&
+              <Grid item xs={12}>
+                <HorseSpecialInstructions
+                  specialInstuctions='These are instructions'
+                />
+              </Grid>
+          }
         </Grid>
       </Card>
     </Grid>
