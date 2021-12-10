ee6c57a63610e3442551ed1eb236ed5721306c6d
Matt Hunt <mstephenhunt@gmail.com>
2021-12-10 08:01:23 -0500

    whitespace cleanup

--- ui/src/HorseCard.tsx
+++ ui/src/HorseCard.tsx
@@ -20,9 +20,6 @@ export default function HorseCard(props: HorseCardProps) {
   return (
     <Grid item xs={12}>
       <Card sx={{ display: 'flex', padding: 1, boxShadow: 'rgba(0, 0, 0, 0.24) 0px 3px 8px' }}>
-
-
-
         <Grid container direction="row">
           <Grid item xs={3}>
             {/*Horse Picture*/}
@@ -41,10 +38,6 @@ export default function HorseCard(props: HorseCardProps) {
                />
             </CardContent>
           </Grid>
-
-
-
-
           <Grid item xs={12} sx={{ display: 'flex', justifyContent: 'center' }}>
             <Box sx={{ display: 'flex', width: 0.95, borderBottom: 0.8, borderColor: '#C4C4C4', marginTop: 0.5, marginBottom: 0.5 }} />
           </Grid>
