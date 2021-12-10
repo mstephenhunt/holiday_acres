30f05e0ce81984caa62c208cfac557e00893d93f
Cliff Hunt <honduq@gmail.com>
2021-12-10 10:35:39 -0500

    changed horses to barn section instead of paddock, added barn_sections model

--- api/holiday_acres_api/models/Horses.py
+++ api/holiday_acres_api/models/Horses.py
@@ -1,6 +1,6 @@
 from django.db import models
 from .Users import User
-from .Paddocks import Paddock
+from .Barn_Sections import Barn_Section
 from django.db.models.deletion import SET_DEFAULT, SET_NULL
 
 
@@ -8,9 +8,9 @@ class Horse(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     # DB RELATIONSHIPS
-    # one-to-many relation (one paddock to many horses)
-    paddock = models.ForeignKey(
-        Paddock, related_name="horses", null=True, on_delete=SET_NULL
+    # one-to-many relation (one Barn_Section to many Horses)
+    barn_section = models.ForeignKey(
+        Barn_Section, related_name="horses", null=True, on_delete=SET_NULL
     )
     # one-to-many relation (one user to many horses)
     user = models.ForeignKey(User, related_name="horses", null=True, on_delete=SET_NULL)
