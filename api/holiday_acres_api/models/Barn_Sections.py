9b7e4df08fcf68c7d1be5bf08c73b2efc973eaa4
Matt Hunt <mstephenhunt@gmail.com>
2021-12-11 10:12:49 -0500

    cleanup

--- api/holiday_acres_api/models/Barn_Sections.py
+++ api/holiday_acres_api/models/Barn_Sections.py
@@ -5,4 +5,3 @@ class Barn_Section(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     name = models.CharField(max_length=200)
-    # related_name = "horses"
