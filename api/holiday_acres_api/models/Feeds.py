071ae3533371e627f0266763274477ac0726d319
Matt Hunt <mstephenhunt@gmail.com>
2021-12-11 10:11:53 -0500

    migration for feed and unit as enums

--- api/holiday_acres_api/models/Feeds.py
+++ api/holiday_acres_api/models/Feeds.py
@@ -1,14 +1,38 @@
 from django.db import models
 from .Horses import Horse
-from django.db.models.deletion import SET_DEFAULT, SET_NULL
+from django.db.models.deletion import SET_NULL
+from django.utils.translation import gettext_lazy as _
 
 
 class Feed(models.Model):
+
+    """
+    Enum defs for FeedTypes/Units
+    """
+
+    class FeedType(models.TextChoices):
+        PELLETS = ("PELLETS",)
+        HAY_PELLETS = ("HAY_PELLETS",)
+        HAY_CUT = ("HAY_CUT",)
+        FIBREMAX = ("FIBREMAX",)
+        ALFALFA = ("ALFALFA",)
+        CARB_SAFE = ("CARB_SAFE",)
+        OIL = "OIL"
+        NONE = ""
+
+    class FeedUnit(models.TextChoices):
+        SCOOP = ("SCOOP",)
+        HANDFUL = ("HANDFUL",)
+        FIRST_CUT = ("FIRST_CUT",)
+        SECOND_CUT = "SECOND_CUT"
+
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    # Feed DATA
-    feed_type = models.CharField(max_length=200)
-    unit = models.CharField(max_length=200)
+
+    feed_type = models.CharField(
+        max_length=200, choices=FeedType.choices, default=FeedType.NONE
+    )
+    unit = models.CharField(max_length=200, choices=FeedUnit.choices, blank=True)
     amount = models.IntegerField(blank=True)
     # one-to-many relation (one user to many horses)
     horse = models.ForeignKey(Horse, related_name="feed", null=True, on_delete=SET_NULL)
