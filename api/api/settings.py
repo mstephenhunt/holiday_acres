52f28ff7f17d46796f7c31a1b2cb99c17a9e962d
Matt Hunt <mstephenhunt@gmail.com>
2021-12-31 11:34:13 -0500

    pull in secret key

--- api/api/settings.py
+++ api/api/settings.py
@@ -36,7 +36,7 @@ DATABASES = {
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-4cqg95p6md-4j4-q+c_b*n3&7#^!f@sg=@1-=85yvr2l_!69%6"
+SECRET_KEY = env("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 if env("ENVIRONMENT") == "development":
