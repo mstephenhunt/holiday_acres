96b2923b36c0356d9c89f18ba06b0a8fb75a70a0
Matt Hunt <mstephenhunt@gmail.com>
2022-01-01 10:48:55 -0500

    card wrapper

--- ui/src/HorseDetailsCard.tsx
+++ ui/src/HorseDetailsCard.tsx
@@ -1,5 +1,6 @@
 import HorseInfoDetailsHeading from './HorseInfoDetailsHeading';
 import { Feed } from "./types";
+import Card from "@mui/material/Card";
 
 type HorseDetailsCardProps = {
   id: number;
@@ -11,10 +12,18 @@ type HorseDetailsCardProps = {
 
 export default function HorseDetailsCard(props: HorseDetailsCardProps) {
   return (
-    <HorseInfoDetailsHeading
-      name={props.name}
-      stall={props.stall}
-      imagePath="some junk"
-    />
+    <Card
+      sx={{
+        display: "flex",
+        padding: 1,
+        boxShadow: "rgba(0, 0, 0, 0.24) 0px 3px 8px",
+      }}
+    >
+      <HorseInfoDetailsHeading
+        name={props.name}
+        stall={props.stall}
+        imagePath="some junk"
+      />
+    </Card>
   );
 }
