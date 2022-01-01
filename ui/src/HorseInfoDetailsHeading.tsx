72a57547d1dbfab6adfdb514adf94c1983a763f0
Matt Hunt <mstephenhunt@gmail.com>
2022-01-01 10:56:43 -0500

    dummy button

--- ui/src/HorseInfoDetailsHeading.tsx
+++ ui/src/HorseInfoDetailsHeading.tsx
@@ -1,6 +1,7 @@
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
 
 type HorseInfoDetailsHeadingProps = {
   name: string;
@@ -28,7 +29,6 @@ export default function HorseInfoDetailsHeading(props: HorseInfoDetailsHeadingPr
         xs={6}
         sx={{
           display: "flex",
-          marginBottom: 2,
           paddingLeft: 2,
           alignSelf: "flex-end",
         }}
@@ -41,13 +41,21 @@ export default function HorseInfoDetailsHeading(props: HorseInfoDetailsHeadingPr
           {props.name}
         </Typography>
       </Grid>
-      <Grid item xs={3} sx={{ display: "flex", marginBottom: 2 }}>
-        <Typography
-          variant="subtitle1"
-          sx={{ height: 28, alignSelf: "flex-end" }}
-        >
-          {props.stall}
-        </Typography>
+      <Grid item xs={3} sx={{ display: "flex" }}>
+        <Grid container>
+          <Grid item>
+            <Button variant="outlined">Edit</Button>
+          </Grid>
+          <Grid item sx={{ alignSelf: "flex-end", }}>
+            <Typography
+              variant="subtitle1"
+              sx={{ height: 28 }}
+            >
+              {props.stall}
+            </Typography>
+          </Grid>
+
+        </Grid>
       </Grid>
     </Grid>
   );
