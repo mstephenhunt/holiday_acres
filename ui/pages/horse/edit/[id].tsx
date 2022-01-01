0b71ee80331aaf199651688c3ed0f55f53b89080
Matt Hunt <mstephenhunt@gmail.com>
2022-01-01 13:27:57 -0500

    edit page

---
+++ ui/pages/horse/edit/[id].tsx
@@ -0,0 +1,56 @@
+import { useRouter } from 'next/router';
+import Container from "@mui/material/Container";
+import ButtonAppBar from "../../../src/ButtonAppBar";
+import HorseEditCard from "../../../src/HorseEditCard";
+import { Horse } from '../../../src/types';
+import { useState, useEffect } from "react";
+
+export default function HorseEdit() {
+  const [horse, setHorse] = useState<Horse>();
+  const { query } = useRouter();
+  const id = query.id;
+
+  const getHorse = async () => {
+    /**
+     * For some reason this page is loading against /horse/undefined a bunch
+     * before getting access to the id from the url ¯\_(ツ)_/¯
+     */
+    if (!id) {
+      return;
+    }
+
+    const response = await fetch(
+      `http://localhost:8000/api/horses/${id}/`.toString(),
+      {}
+    );
+    const horse = await response.json();
+
+    if (horse && horse.detail !== 'Not found.') {
+      setHorse(horse as Horse);
+    }
+  };
+
+  useEffect(() => {
+    if (!horse) {
+      getHorse();
+    }
+  });
+
+  if (!horse) {
+    return <h1>Loading...</h1>
+  } else {
+    return (
+      <Container disableGutters>
+        <ButtonAppBar />
+        <Container sx={{ py: 2 }} maxWidth="md">
+          <HorseEditCard
+            id={horse.id}
+            name={horse.name}
+            stall={horse.stall}
+            feed={horse.feed}
+            specialInstructions={horse.special_instructions}
+          />
+        </Container>
+      </Container>)
+  }
+}
---
+++ ui/src/HorseInfoEditHeading.tsx
@@ -0,0 +1,61 @@
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+
+type HorseInfoEditHeadingProps = {
+  name: string;
+  stall: string;
+  imagePath: string;
+};
+
+export default function HorseInfoEditHeading(props: HorseInfoEditHeadingProps) {
+  return (
+    <Grid container spacing={0}>
+      <Grid item xs={3}>
+        <CardMedia
+          component="img"
+          sx={{
+            width: 80,
+            height: 80,
+            boxShadow: "rgba(0, 0, 0, 0.4) 0px 3px 8px",
+          }}
+          image="https://source.unsplash.com/random"
+          alt={props.name}
+        />
+      </Grid>
+      <Grid
+        item
+        xs={6}
+        sx={{
+          display: "flex",
+          paddingLeft: 2,
+          alignSelf: "flex-end",
+        }}
+      >
+        <Typography
+          component="h4"
+          variant="h4"
+          sx={{ fontWeight: 600, height: 40 }}
+        >
+          {props.name}
+        </Typography>
+      </Grid>
+      <Grid item xs={3} sx={{ display: "flex" }}>
+        <Grid container>
+          <Grid item>
+            <Button variant="contained">Save</Button>
+          </Grid>
+          <Grid item sx={{ alignSelf: "flex-end", }}>
+            <Typography
+              variant="subtitle1"
+              sx={{ height: 28 }}
+            >
+              {props.stall}
+            </Typography>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
+  );
+}
