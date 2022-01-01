fafd26511d198324b245dc8ab77225ec16aa86f0
Matt Hunt <mstephenhunt@gmail.com>
2022-01-01 11:01:41 -0500

    line

--- ui/src/HorseDetailsCard.tsx
+++ ui/src/HorseDetailsCard.tsx
@@ -1,6 +1,8 @@
 import HorseInfoDetailsHeading from './HorseInfoDetailsHeading';
 import { Feed } from "./types";
 import Card from "@mui/material/Card";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
 
 type HorseDetailsCardProps = {
   id: number;
@@ -19,11 +21,27 @@ export default function HorseDetailsCard(props: HorseDetailsCardProps) {
         boxShadow: "rgba(0, 0, 0, 0.24) 0px 3px 8px",
       }}
     >
-      <HorseInfoDetailsHeading
-        name={props.name}
-        stall={props.stall}
-        imagePath="some junk"
-      />
+      <Grid container direction="row">
+        <Grid item xs={12}>
+          <HorseInfoDetailsHeading
+            name={props.name}
+            stall={props.stall}
+            imagePath="some junk"
+          />
+        </Grid>
+        <Grid item xs={12} sx={{ display: "flex", justifyContent: "center" }}>
+          <Box
+            sx={{
+              display: "flex",
+              width: 0.95,
+              borderBottom: 0.8,
+              borderColor: "#C4C4C4",
+              marginTop: 0.5,
+              marginBottom: 0.5,
+            }}
+          />
+        </Grid>
+      </Grid>
     </Card>
   );
 }
