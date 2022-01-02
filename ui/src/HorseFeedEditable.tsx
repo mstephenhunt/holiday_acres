dc383902646c40043535a442afe7bcbfee608e20
Matt Hunt <mstephenhunt@gmail.com>
2022-01-02 12:09:09 -0500

    width on seletor

--- ui/src/HorseFeedEditable.tsx
+++ ui/src/HorseFeedEditable.tsx
@@ -6,6 +6,7 @@ import FormControl from '@mui/material/FormControl';
 import InputLabel from '@mui/material/InputLabel';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
 import MenuItem from '@mui/material/MenuItem';
+import Grid from '@mui/material/Grid';
 
 type HorseFeedEditableComponentProps = {
   feed?: Feed;
@@ -19,25 +20,29 @@ export default function HorseFeedEditable(props: HorseFeedEditableComponentProps
   };
 
   return (
-    <Box>
-      <FormControl fullWidth>
-        <InputLabel id="feed-select-label">Feed</InputLabel>
-        <Select
-          labelId="feed-select-label"
-          // id="demo-simple-select" // what is this for?
-          value={feedLabel}
-          label="Feed Type"
-          onChange={handleFeedTypeChange}
-        >
-          <MenuItem value={FeedType.PELLETS}>{feedTypeToLabelMap.get(FeedType.PELLETS)}</MenuItem>
-          <MenuItem value={FeedType.HAY_PELLETS}>{feedTypeToLabelMap.get(FeedType.HAY_PELLETS)}</MenuItem>
-          <MenuItem value={FeedType.HAY_CUT}>{feedTypeToLabelMap.get(FeedType.HAY_CUT)}</MenuItem>
-          <MenuItem value={FeedType.FIBREMAX}>{feedTypeToLabelMap.get(FeedType.FIBREMAX)}</MenuItem>
-          <MenuItem value={FeedType.ALFALFA}>{feedTypeToLabelMap.get(FeedType.ALFALFA)}</MenuItem>
-          <MenuItem value={FeedType.CARB_SAFE}>{feedTypeToLabelMap.get(FeedType.CARB_SAFE)}</MenuItem>
-          <MenuItem value={FeedType.OIL}>{feedTypeToLabelMap.get(FeedType.OIL)}</MenuItem>
-        </Select>
-      </FormControl>
-    </Box>
+    <Grid container xs={12}>
+      <Grid item xs={5}>
+        <Box>
+          <FormControl fullWidth>
+            <InputLabel id="feed-select-label">Feed</InputLabel>
+            <Select
+              labelId="feed-select-label"
+              // id="demo-simple-select" // what is this for?
+              value={feedLabel}
+              label="Feed Type"
+              onChange={handleFeedTypeChange}
+            >
+              <MenuItem value={FeedType.PELLETS}>{feedTypeToLabelMap.get(FeedType.PELLETS)}</MenuItem>
+              <MenuItem value={FeedType.HAY_PELLETS}>{feedTypeToLabelMap.get(FeedType.HAY_PELLETS)}</MenuItem>
+              <MenuItem value={FeedType.HAY_CUT}>{feedTypeToLabelMap.get(FeedType.HAY_CUT)}</MenuItem>
+              <MenuItem value={FeedType.FIBREMAX}>{feedTypeToLabelMap.get(FeedType.FIBREMAX)}</MenuItem>
+              <MenuItem value={FeedType.ALFALFA}>{feedTypeToLabelMap.get(FeedType.ALFALFA)}</MenuItem>
+              <MenuItem value={FeedType.CARB_SAFE}>{feedTypeToLabelMap.get(FeedType.CARB_SAFE)}</MenuItem>
+              <MenuItem value={FeedType.OIL}>{feedTypeToLabelMap.get(FeedType.OIL)}</MenuItem>
+            </Select>
+          </FormControl>
+        </Box>
+      </Grid>
+    </Grid>
   );
 }
