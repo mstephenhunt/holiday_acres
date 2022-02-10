da95b0223108f24a9e34f40872eb7d7cc0ebbaee
Matt Hunt <mstephenhunt@gmail.com>
2022-02-10 16:52:01 -0500

    create user

--- haec-user-service/src/modules/user/controllers/user.controller.ts
+++ haec-user-service/src/modules/user/controllers/user.controller.ts
@@ -1,9 +1,38 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Header, Body } from '@nestjs/common';
+import { UserService } from '../services/user.service';
+
+type CreateUserDto = {
+  email: string;
+  name?: string;
+}
+
+type SerializedUser = {
+  id: number;
+  email: string;
+  name?: string;
+}
 
 @Controller()
 export class UserController {
+  constructor(private userService: UserService) {}
+
   @Get('/user/test')
   getHello(): string {
     return 'yoyo';
+  }
+
+  @Post('/user')
+  @Header('content-type', 'application/json')
+  async createUser(@Body() createUserDto: CreateUserDto): Promise<SerializedUser> {
+    const createdUser = await this.userService.createUser({
+      email: createUserDto.email,
+      name: createUserDto.name
+    });
+
+    return {
+      id: createdUser.id,
+      email: createdUser.email,
+      name: createdUser.name
+    }
   }
 }
