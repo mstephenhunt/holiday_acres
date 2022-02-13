a7056b3733a8c7ef05b982a118e0c557b8eff1d0
Matt Hunt <mstephenhunt@gmail.com>
2022-02-13 10:15:38 -0500

    remove nanoid

--- haec-user-service/test/prisma-test-environment.ts
+++ haec-user-service/test/prisma-test-environment.ts
@@ -2,21 +2,16 @@
 
 import { PrismaClient } from '@prisma/client';
 import { Config } from '@jest/types';
-import { nanoid } from 'nanoid';
 import { ConfigService } from '@nestjs/config';
 const NodeEnvironment = require('jest-environment-node');
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 
 class PrismaTestEnvironment extends NodeEnvironment {
-  schema: string;
   databaseUrl: string;
 
   constructor(config: Config.ProjectConfig) {
     super(config);
-
-    // Generate a unique schema identifier for this text context
-    this.schema = `test_${nanoid()}`;
 
     // Generate the pg connection string for ther test schema
     this.databaseUrl = 'file:./test.db';
