8bc40a90796b88e9c6b7b7040c45f63da638ada3
Cliff Hunt <honduq@gmail.com>
2022-03-09 05:59:07 -0500

    views cleanup

--- api/holiday_acres_api/views.py
+++ api/holiday_acres_api/views.py
@@ -14,8 +14,6 @@ from django.http import JsonResponse, HttpResponse
 from datetime import datetime
 import requests
 
-# import requests
-
 
 class UserViewSet(viewsets.ModelViewSet):
     """
@@ -56,7 +54,7 @@ class BarnSectionViewSet(viewsets.ModelViewSet):
 @api_view(["POST"])
 def register_account_request(request):
     """
-    Passes data from the /users/register route to  Used to create new users in the database.
+    Passes data from the /users/register route to 3001/user to create new users in the database.
     """
     body = request.data
     response = HttpResponse()
@@ -67,56 +65,6 @@ def register_account_request(request):
     )
 
     return response
-
-    # # If required fields are missing, return 500 through api
-    # if (
-    #     "username" not in body
-    #     or "password" not in body
-    #     or "password_confirmation" not in body
-    #     or "email" not in body
-    #     or "first_name" not in body
-    #     or "last_name" not in body
-    # ):
-    #     response.status_code = 500
-    #     response.reason_phrase = "Missing required fields to register user"
-    #     return response
-
-    # username = body["username"]
-    # password = body["password"]
-    # password_confirmation = body["password_confirmation"]
-    # email = body["email"]
-    # first_name = body["first_name"]
-    # last_name = body["last_name"]
-
-    # # If the passwords don't match, reject the creation
-    # if password != password_confirmation:
-    #     response.status_code = 500
-    #     response.reason_phrase = "Password and password confirmation don't match"
-    #     return response
-
-    # # Does this user exist in the database?
-    # queryset = User.objects.all()
-    # # iterate through each User instance, check object.username against username variable
-    # for user in queryset:
-    #     if user.username == username:
-    #         print("username already exists, dude")
-    #         response.status_code = 500
-    #         response.reason_phrase = "Username already exists"
-    #         return response
-
-    # # All good, create the user
-    # new_user = User.objects.create(
-    #     email=email,
-    #     username=username,
-    #     password=password,
-    #     first_name=first_name,
-    #     last_name=last_name,
-    # )
-    # new_user.save()
-
-    # response.status_code = 200
-    # response.reason_phrase == "New user created"
-    # return response
 
 
 @api_view(["GET"])
