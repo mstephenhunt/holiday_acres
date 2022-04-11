667b71142666ce1bcc88cc2639bb3bfac8e52696
Cliff Hunt <honduq@gmail.com>
2022-04-10 21:55:57 -0400

    Module './modules/user/services/user.service' has no exported member 'verifyUser'.ts(2305)

--- haec-user-service/src/app.module.ts
+++ haec-user-service/src/app.module.ts
@@ -18,18 +18,3 @@ export class AppModule implements NestModule {
       .forRoutes(UserController);
   }
 }
-
-// import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
-// import { LoggerMiddleware } from './common/middleware/logger.middleware';
-// import { CatsModule } from './cats/cats.module';
-
-// @Module({
-//   imports: [CatsModule],
-// })
-// export class AppModule implements NestModule {
-//   configure(consumer: MiddlewareConsumer) {
-//     consumer
-//       .apply(LoggerMiddleware)
-//       .forRoutes('cats');
-//   }
-// }
