3d0805db6e9c145a1276a4ca5507adebfacec3d8
Matt Hunt <mstephenhunt@gmail.com>
2022-04-19 19:18:25 -0400

    a change

--- haec-user-service/src/app.module.ts
+++ haec-user-service/src/app.module.ts
@@ -3,6 +3,7 @@ import { UserModule } from './modules/user/user.module';
 import { PrismaService } from './providers/prisma.service';
 import { ConfigModule } from '@nestjs/config';
 
+// this is a change
 @Module({
   imports: [UserModule, ConfigModule.forRoot({ isGlobal: true, cache: true })],
   controllers: [],
