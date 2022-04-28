d916730b28d6a165034cd379f4dfdbfc5cd4e527
Cliff Hunt <honduq@gmail.com>
2022-04-27 21:42:47 -0400

    changed loggermiddleware to authenticationmiddleware

--- haec-user-service/src/_middleware.ts
+++ haec-user-service/src/_middleware.ts
@@ -2,16 +2,16 @@ import { Injectable, NestMiddleware, Request, Response } from '@nestjs/common';
 import { NextFunction } from 'express';
 
 @Injectable()
-export class LoggerMiddleware implements NestMiddleware {
+export class AuthenticationMiddleware implements NestMiddleware {
 
   use(req: Request, res: Response, next: NextFunction) {
     console.log("Middleware Authentication is running");
-    // Middleware Authentication FAIL
+    // Middleware Authentication SUCCESS
     if (req.headers["haec-auth-token"] === "neither-is-this-a-secret-key") {
       console.log("Middleware Authentication Passed");
       next();
     }
-    // Middleware Authentication SUCCESS
+    // Middleware Authentication FAIL
     else {
       throw("Middleware Authentication Failed")
       }
--- haec-user-service/src/app.module.ts
+++ haec-user-service/src/app.module.ts
@@ -4,7 +4,7 @@ import { UserService } from './modules/user/services/user.service';
 import { UserController } from './modules/user/controllers/user.controller';
 import { PrismaService } from './providers/prisma.service';
 import { ConfigModule } from '@nestjs/config';
-import { LoggerMiddleware } from './_middleware';
+import { AuthenticationMiddleware } from './_middleware';
 
 
 @Module({
@@ -15,7 +15,7 @@ import { LoggerMiddleware } from './_middleware';
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
     consumer
-      .apply(LoggerMiddleware)
+      .apply(AuthenticationMiddleware)
       .forRoutes(UserController);
   }
 }
