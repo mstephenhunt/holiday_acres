05cf128246ba01b2a534490c6b0c1cb4fffd704b
Sean Welsh <s.welsh1314@gmail.com>
2022-07-05 19:57:58 -0400

    small changes to Feed

--- barn-api-service/modules/barn/models/BarnSection.py
+++ barn-api-service/modules/barn/models/BarnSection.py
@@ -1,28 +1,28 @@
 from decimal import Decimal
 from database import Base
-from sqlalchemy import Column, Integer, String, Numeric, BigInteger
+from sqlalchemy import Column, Integer, String, Float
 
 
 class BarnSection(Base):
     __tablename__ = "holiday_acres_api_barn_section"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, unique=True, index=True)
+    name = Column(String)
 
 
 class Horse(Base):
     __tablename__ = "holiday_acres_api_horse"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, unique=True, index=True)
+    name = Column(String)
 
 class Feed(Base):
     __tablename__ = "holiday_acres_api_feed"
     id = Column(Integer, primary_key=True, index=True)
-    feed_type = Column(String, unique=True, index=True)
-    amount = Column(Numeric, unique=True, index=True)
-    horse_id = Column(BigInteger, unique=True, index=True)
-    unit = Column(String, unique=True, index=True)
+    feed_type = Column(String)
+    amount = Column(Float)
+    horse_id = Column(Integer)
+    unit = Column(String)
 
 
 
