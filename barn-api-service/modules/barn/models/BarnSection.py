427c222c94836729733f507afd6dc5bc6abd8e5e
Toxicity1314 <s.welsh1314@gmail.com>
2022-07-19 20:00:42 -0400

    matt fix please

--- barn-api-service/modules/barn/models/BarnSection.py
+++ barn-api-service/modules/barn/models/BarnSection.py
@@ -1,19 +1,22 @@
 from database import Base
-from sqlalchemy import Column, Integer, String, Float
+from sqlalchemy import Column, Integer, String, Float, Table, ForeignKey
+from sqlalchemy.orm import declarative_base, relationship
 
+Base = declarative_base()
 
 class BarnSection(Base):
     __tablename__ = "holiday_acres_api_barn_section"
-
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String)
+    horses = relationship("Horse")
 
 
 class Horse(Base):
     __tablename__ = "holiday_acres_api_horse"
-
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String)
+    barn_section_id = Column(Integer, ForeignKey("holiday_acres_api_barn_section.id"))
+
 
 class Feed(Base):
     __tablename__ = "holiday_acres_api_feed"
--- barn-api-service/modules/barn/schemas/barn_schema.py
+++ barn-api-service/modules/barn/schemas/barn_schema.py
@@ -1,9 +1,15 @@
-from pydantic import BaseModel
+from sqlalchemy.orm import Query
+from pydantic import BaseModel, conlist, ValidationError
+from typing import List
+from modules.barn.schemas.horse_schema import HorseSchema 
+from modules.barn.models.BarnSection import Horse
 
 
 class BarnSchema(BaseModel):
     id: int
     name: str
+    horse: List[Horse]
 
     class Config:
         orm_mode = True
+        arbitrary_types_allowed = True
